08315f9e147679f219793e3eab6b78be792b425b
fsadkjl <904822026@qq.com>
2017-06-21 10:04:11 +0800

    更改了map迭代顺序

--- src/com/wjl/map/TestMap.java
+++ src/com/wjl/map/TestMap.java
@@ -13,14 +13,20 @@ public class TestMap {
 		map.put("1", "value1");
 		map.put("2", "value2");
 		map.put("3", "value3");
+		
+		// 第一种: 只能遍历value
+		System.out.println("通过Map.values()遍历所有的value，但不能遍历key");
+		for (String v : map.values()) {
+			System.out.println("value= " + v);
+		}
 
-		// 第一种: 普遍使用，二次取值
+		// 第二种: 普遍使用，可以取key和value
 		System.out.println("通过Map.keySet遍历key和value：");
 		for (String key : map.keySet()) {
 			System.out.println("key= " + key + " and value= " + map.get(key));
 		}
 
-		// 第二种: 通过Iterator迭代器遍历循环Map.entrySet().iterator();
+		// 第三种: 通过Iterator迭代器遍历循环Map.entrySet().iterator();
 		System.out.println("通过Map.entrySet使用iterator遍历key和value：");
 		Iterator<Entry<String, String>> it = map.entrySet().iterator();
 		while (it.hasNext()) {
@@ -28,16 +34,12 @@ public class TestMap {
 			System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
 		}
 
-		// 第三种: 推荐，尤其是容量大时
+		// 第种: 推荐，尤其是容量大时
 		System.out.println("通过Map.entrySet遍历key和value");
 		for (Entry<String, String> entry : map.entrySet()) {
 			System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
 		}
 
-		// 第四种: 
-		System.out.println("通过Map.values()遍历所有的value，但不能遍历key");
-		for (String v : map.values()) {
-			System.out.println("value= " + v);
-		}
+		
 	}
 }
